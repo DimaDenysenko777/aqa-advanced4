14acf0ecc2cff601bab1d24e00dcb4e1f3f43aec
DIMADENYSENKO777 <dimakonstantinov29.05.92@gmail.com>
2024-12-16 01:19:04 -0800

    Completed JS HW 4.1

---
+++ jsBasics/task4_1.js
@@ -0,0 +1,3 @@
+const radius = 5;
+const area = Math.PI * radius ** 2;
+console.log("Area of circle:", area);
---
+++ jsBasics/task4_3.js
@@ -0,0 +1,4 @@
+const radius = 3;
+const height = 7;
+const volume = Math.PI * radius ** 2 * height;
+console.log("Volume of cylinder:", volume.toFixed(2));
